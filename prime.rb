21d1134152648f4ba4b3a9e928c4f1e6015c5d85
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-09 13:07:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,8 @@
 def prime?(integer)
   if integer == -1 || integer == 0 || integer == 1
     return false
-    break
   elsif integer == 2
     return true
-    break
   else
     divisors = (2..500).to_a
     divisors.each do |divisor|
